8dea5a7b214a1e6344162d9d3e4cdd58e1712497
Maaz Irshad Siddiqui <85389149+maazsiddiqui79@users.noreply.github.com>
2025-03-20 02:21:21 +0530

    Initail Commit: Added Blind Bidding in  python

---
+++ Project12_Online_Bidding_platform.py
@@ -0,0 +1,41 @@
+from os import system
+auction_db = {}
+
+
+
+
+print("""
+                           ____________
+                            \        /
+                            )-------(
+                            |"""""""|_.-._,.---------.,_.-._
+                            |       | | |               | | ''-.
+                            |       |_| |_             _| |_..-'
+                            |_______| '-'''---------'' '-'
+                            )-------(
+                           /_________\\
+       *BANG!* 💥  ===>  .-------------.
+                        /_______________\\
+                          
+      """)
+
+end =False
+while not end :
+    user_name = input("Enter Your name: ")
+    user_bid_amt = int(input("What is your Bid $: "))
+    auction_db[user_name] = user_bid_amt
+    system("cls")
+    # print(auction_db)
+    continue_bid = int(input("Is There any other bidders? Type '1' for yes or '0' no: "))
+    if continue_bid == 1:
+         pass
+    else:
+        system("cls") # Clears the terminal
+        print(f"The Winner is {max(auction_db,key=auction_db.get)} by {max(auction_db.values())} $")
+        
+        end = True
+    
+         
+     
+    
+    
