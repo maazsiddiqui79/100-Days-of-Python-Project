3f1558ca1e1676de1143b4e3a89f0e5b32bf97fb
Maaz Irshad Siddiqui <85389149+maazsiddiqui79@users.noreply.github.com>
2025-03-26 22:18:41 +0530

    Second Commit: Updated code with color and made the game endless

--- Project17_Number_Guessin-game.py
+++ Project17_Number_Guessin-game.py
@@ -1,4 +1,6 @@
-import random
+import random,colorama
+from colorama import Fore,Style,Back,init
+colorama.init(autoreset=True)
 print('''
 
  __   _ _     _ _______ ______  _______  ______     
@@ -18,35 +20,35 @@ print('''
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
-user_ip = int(input("Choose Your Diffculty. 1 = 'easy' 2 = 'hard': "))
-num = random.randint(1,100)
-print(num)
+while True:
+    user_ip = int(input("Choose Your Diffculty. 1 = 'easy' 2 = 'hard': "))
+    num = random.randint(1,100)
+    print(num)
 
-life = 10 if user_ip == 1 else 5
- 
-
-        
-# game = True
-# while game:
-
-print(f"You have {life} attempts remaining to guess the number.")
+    life = 10 if user_ip == 1 else 5
     
-while life != 0:
-    user_num_ip = int(input("Guess a Number: "))
-    
-    if user_num_ip == num-1 or user_num_ip == num+1 :
-        print("Too Close!")
-    elif user_num_ip < num:
-        print("Too Low!")
-    elif user_num_ip > num:
-        print("Too High!")
-    else:
-        print("🔥 Bingo! You got the correct number!")
-        break
-    life -= 1
-    print(f"You have {life} attempts remaining to guess the number.")
-    if life == 0:
-        print("🔴 Game Over! You couldn't guess the right number.")
-        break
+
+            
+
+    print("You have ",Fore.RED + str(life) ,"attempts remaining to guess the number.")
         
+    while life != 0:
+        user_num_ip = int(input("Guess a Number: "))
         
+        if user_num_ip == num-1 or user_num_ip == num+1 :
+            print("Too Close!")
+        elif user_num_ip < num:
+            print("Too Low! ,Think Bigg")
+        elif user_num_ip > num:
+            print("Too High! , Think Smalll")
+        else:
+            print(Fore.GREEN+"🔥 Bingo!",",You got the correct number!")
+            break
+        life -= 1
+        print("You have",Fore.RED + str(life) ," attempts remaining to guess the number.")
+        if life == 0:
+            print(Fore.RED+"🔴 Game Over! You couldn't guess the right number.")
+            print(Fore.GREEN+Back.LIGHTYELLOW_EX+f"\tThe Correct Number was {user_num_ip}")
+            break
+            
+            
